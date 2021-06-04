4ceffab3d93ae5e0b7cc437a5ca8b332917d443a
TRONDGAMINGRIG\Trond <trond___g@hotmail.com>
2021-06-04 22:57:00 +0200

    Added TV

---
+++ House Model/Assets/Code/Components/Purchasable.cs
@@ -0,0 +1,16 @@
+using System;
+using UnityEngine;
+
+namespace Code.Components
+{
+	public class Purchasable : MonoBehaviour
+	{
+
+		[SerializeField]
+		private string name;
+		
+		[SerializeField]
+		private string url;
+
+	}
+}
